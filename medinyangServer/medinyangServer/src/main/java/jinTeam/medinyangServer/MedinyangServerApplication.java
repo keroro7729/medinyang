0964599fdbed4ccd01b5674f4521d7594d35471d
jinwook kim <keroro06108@gmail.com>
2025-04-03 22:51:09 +0900

    server boot

---
+++ medinyangServer/medinyangServer/src/main/java/jinTeam/medinyangServer/MedinyangServerApplication.java
@@ -0,0 +1,13 @@
+package jinTeam.medinyangServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedinyangServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedinyangServerApplication.class, args);
+	}
+
+}
---
+++ medinyangServer/medinyangServer/src/test/java/jinTeam/medinyangServer/MedinyangServerApplicationTests.java
@@ -0,0 +1,13 @@
+package jinTeam.medinyangServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedinyangServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
