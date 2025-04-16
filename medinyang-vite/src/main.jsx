47f0cde17d4be951e9e51a0c406552977df13d84
Mungwani <gwcowani@kangwon.ac.kr>
2025-04-16 21:47:59 +0900

    feat: 로그인 페이지 구현

--- medinyang-vite/src/main.jsx
+++ medinyang-vite/src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId="YOUR_GOOGLE_CLIENT_ID"> {/* 👈 여기 추가 */}
+      <App />
+    </GoogleOAuthProvider>
   </React.StrictMode>,
 );
