8ebf0435153cd909bc7f63fd55746ff69288209d
Mungwani <gwcowani@kangwon.ac.kr>
2025-05-08 23:08:27 +0900

    chore: Vite proxy 설정 추가 및 WebSocket 프록시 연동

--- medinyang-vite/vite.config.js
+++ medinyang-vite/vite.config.js
@@ -5,4 +5,17 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   host: true,
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/ws': {
+        target: 'http://localhost:8080',
+        ws: true,
+        changeOrigin: true,
+      },
+      '/login': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+      }
+    }
+  }
+});
