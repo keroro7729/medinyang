a439c5f0497b3e80798e80ca1649b2dc2df356b4
YOOJIIN <yoojiin0328@naver.com>
2025-05-08 22:00:26 +0900

    refactor: controller 분리

--- medinyangServer/medinyangServer/src/main/java/jinTeam/medinyangServer/controller/ImageFileController.java
+++ medinyangServer/medinyangServer/src/main/java/jinTeam/medinyangServer/controller/ImageFileController.java
@@ -1,11 +1,36 @@
 package jinTeam.medinyangServer.controller;
 
+import jinTeam.medinyangServer.database.imageFile.ImageFile;
+import jinTeam.medinyangServer.database.imageFile.ImageFileService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/image-files")
+@RequiredArgsConstructor
 @Slf4j
 public class ImageFileController {
+
+    private final ImageFileService imageFileService;
+
+    @PostMapping(value = "/upload", consumes = "multipart/form-data")
+    public ResponseEntity<Map<String, Object>> uploadImageFile(@RequestPart("file") MultipartFile file) {
+        ImageFile saved = imageFileService.uploadImage(file);
+
+        log.info("파일 업로드 성공: {}", file.getOriginalFilename());
+
+        return ResponseEntity.ok(Map.of(
+                "message", "업로드 성공!",
+                "id", saved.getImageId()
+        ));
+    }
+
 }
