e21fe83100ef686f9ace5a6b592a1cdef82f231a
keroro729 <keroro06108@gmail.com>
2025-05-09 23:55:08 +0900

    .

--- medinyangServer/medinyangServer/src/main/java/jinTeam/medinyangServer/controller/ImageFileController.java
+++ medinyangServer/medinyangServer/src/main/java/jinTeam/medinyangServer/controller/ImageFileController.java
@@ -2,7 +2,7 @@ package jinTeam.medinyangServer.controller;
 
 import jinTeam.medinyangServer.database.imageFile.ImageFile;
 import jinTeam.medinyangServer.database.imageFile.ImageFileService;
-<<<<<<< HEAD
+import jinTeam.medinyangServer.dto.response.DefaultResponseDto;
 import jinTeam.medinyangServer.enums.ImageType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,8 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDate;
-=======
-import jinTeam.medinyangServer.dto.response.DefaultResponse;
+import jinTeam.medinyangServer.dto.response.DefaultResponseDto;
 import jinTeam.medinyangServer.dto.response.UploadImageResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -23,34 +22,19 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
->>>>>>> 021d1176193b63266a6053853ce08edfd479d199
 import java.util.Map;
 
 @RestController
 @RequestMapping("/image-files")
 @RequiredArgsConstructor
 @Slf4j
-@RequiredArgsConstructor
 public class ImageFileController {
 
     private final ImageFileService imageFileService;
 
     @PostMapping(value = "/upload", consumes = "multipart/form-data")
-<<<<<<< HEAD
-    public ResponseEntity<?> upload(@RequestParam Long userId, @RequestPart("file") MultipartFile file) {
-        ImageFile saved = imageFileService.uploadImage(file, userId);
 
-        log.info("파일 업로드 성공: {}", file.getOriginalFilename());
-
-        // OCR 추출 LLM 파싱
-        imageFileService.addDetail(saved, "예시 설명", "예시 병원", LocalDate.of(2025, 5, 7), ImageType.PRESCRIPTION);
-
-        return ResponseEntity.ok(Map.of(
-                "message", "업로드 성공!",
-                "id", saved.getImageId()
-        ));
-=======
-    public ResponseEntity<DefaultResponse<UploadImageResponse>> uploadImageFile(@RequestPart("file") MultipartFile file) {
+    public ResponseEntity<DefaultResponseDto<UploadImageResponse>> uploadImageFile(@RequestPart("file") MultipartFile file) {
         ImageFile saved = imageFileService.uploadImage(file);
 
         log.info("파일 업로드 성공: {}", file.getOriginalFilename());
@@ -59,8 +43,7 @@ public class ImageFileController {
 
         UploadImageResponse uploadImageData = new UploadImageResponse(imageId);
 
-        return ResponseEntity.ok(new DefaultResponse<>(true,"이미지 업로드 성공!", uploadImageData));
->>>>>>> 021d1176193b63266a6053853ce08edfd479d199
+        return ResponseEntity.ok(new DefaultResponseDto<>(true,"이미지 업로드 성공!", uploadImageData));
     }
 
 }
