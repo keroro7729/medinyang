d8c7c3f80cf2950426be3afb27c2daf5fd88a78b
YOOJIIN <yoojiin0328@naver.com>
2025-05-27 22:52:01 +0900

    feat: 성공/실패에 따른 반환 메소드 추가

--- medinyangServer/medinyangServer/src/main/java/jinTeam/medinyangServer/common/dto/response/DefaultResponseDto.java
+++ medinyangServer/medinyangServer/src/main/java/jinTeam/medinyangServer/common/dto/response/DefaultResponseDto.java
@@ -3,6 +3,7 @@ package jinTeam.medinyangServer.common.dto.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @Getter
 @AllArgsConstructor
@@ -10,5 +11,13 @@ public class DefaultResponseDto<T> {
     private int status;
     private String message;
     private T data;
+
+    public static <T> DefaultResponseDto<T> success(T data) {
+        return new DefaultResponseDto<>(HttpStatus.OK.value(), "요청 성공", data);
+    }
+
+    public static <T> DefaultResponseDto<T> fail(int status, String message) {
+        return new DefaultResponseDto<>(status, message, null);
+    }
 }
 
